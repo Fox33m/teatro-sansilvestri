9ab00850b8de9456cca1fc1ce9fecef29582518b
Fox33m <marco.sansilvestri@gmail.com>
2022-08-26 12:17:27 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# teatro-sansilvestri
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/teatro-sansilvestri)
---
+++ src/app/app.component.ts
@@ -0,0 +1,68 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  movieTitle:string = "Prenotazione posti";
+    screen: string = "Realizzato da";
+    time: string = "Marco Sansilvestri"
+    price:number;
+    tick:number;
+    conv:number;
+    rows: string[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+    cols: number[]  = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    reserved: string[] = ['A2', 'A3', 'F1', 'F2', 'F5', 'F6', 'F7', 'F8'];
+    selected: string[] = [];
+
+    ticketPrice: number = 10;
+    convFee: number = 5;
+    totalPrice: number = 0;
+    currency: string = "€";
+
+    //riporta lo stato dei posti
+    getStatus = function(seatPos: string) {
+        if(this.reserved.indexOf(seatPos) !== -1) {
+            return 'reserved';
+        } else if(this.selected.indexOf(seatPos) !== -1) {
+            return 'selected';
+        }
+    }
+    //cancella selezione
+    clearSelected = function() {
+        this.selected = [];
+    }
+     //prenota i posti
+     bookSelected = function() {
+        this.reserved = this.reserved+this.selected;
+        this.selected = [];
+    }
+     
+    //interazione con i posti
+    seatClicked = function(seatPos: string) {
+        var index = this.selected.indexOf(seatPos);
+        
+        if(index !== -1) {
+            // rimuovi un posto selezionato ricliccandoci
+            this.selected.splice(index, 1)
+        } else {
+            //permetti la selezione solo dei posti liberi
+            if(this.reserved.indexOf(seatPos) === -1)
+                this.selected.push(seatPos);
+        }
+    }
+    //informazioni prenotazione
+    showSelected = function() {
+        if(this.selected.length > 0) {
+            
+            this.price=this.ticketPrice;
+           this.tick=this.selected.length;
+            this.conv=this.convFee;
+        } else {
+            alert("Nessun posto selezionato!");
+        }
+    }   
+}
